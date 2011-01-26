2cfed66470104968d5421167c3a258b3c9105e10
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-01-25 17:27:51 -0800

    docs

--- lib/App/cpanlistchanges.pm
+++ lib/App/cpanlistchanges.pm
@@ -112,6 +112,9 @@ App::cpanlistchanges - list changes for CPAN modules
 
 Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
 
+Tokuhiro Matsuno originally wrote the snippet to fetch Changes and
+compare with Algorithm::Diff if I remember correctly.
+
 =head1 COPYRIGHT
 
 Copyright 2010- Tatsuhiko Miyagawa
